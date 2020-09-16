dfb00426beb2004fbed15883317a61d2917d7e38
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-16 12:02:09 +0100

    initial commit created pub class and pub init tests

---
+++ tests/pub_test.py
@@ -0,0 +1,15 @@
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("Ox", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("Ox", self.pub.name)
+
+    def test_pub_has_till(self):
+        self.assertEqual(100.00, self.pub.till)
+
+    def test_pub_drinks_empty(self):
+        self.assertEqual(0,len(self.pub.drinks))
